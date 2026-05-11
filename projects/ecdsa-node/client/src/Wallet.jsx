49b251b4bf5526a666996c08c2e92518ba465b53
Jorge Tome Blanco <jorge.tome@codere.com>
2026-05-11 12:31:03 +0200

    feat: add Private Key input + show balance based on private key instead of address directly

--- projects/ecdsa-node/client/src/Wallet.jsx
+++ projects/ecdsa-node/client/src/Wallet.jsx
@@ -1,9 +1,28 @@
 import server from "./server";
+import * as secp from 'ethereum-cryptography/secp256k1';
+import { hexToBytes, toHex } from "ethereum-cryptography/utils";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
   async function onChange(evt) {
     const address = evt.target.value;
     setAddress(address);
+    if (address) {
+      const {
+        data: { balance },
+      } = await server.get(`balance/${address}`);
+      setBalance(balance);
+    } else {
+      setBalance(0);
+    }
+  }
+
+  async function onPrivateKeyChange(evt) {
+    const privateKey = evt.target.value;
+    console.log(`pre-set ${privateKey}`);
+    setPrivateKey(privateKey);
+    console.log(`post-set ${privateKey}`);
+    const publicKeyBytes = secp.getPublicKey(hexToBytes(privateKey));
+    const address = '0x' + toHex(publicKeyBytes).slice(-20)
     if (address) {
       const {
         data: { balance },
@@ -21,6 +40,11 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <label>
         Wallet Address
         <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+      </label>
+
+      <label>
+        Wallet Private Key
+        <input placeholder="Type a private key" value={privateKey} onChange={onPrivateKeyChange}></input>
       </label>
 
       <div className="balance">Balance: {balance}</div>

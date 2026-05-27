66c7f3c35f87bbe44e7133ca49d8dc6a03cc3d18
Jorge Tome Blanco <jorge.tome@codere.com>
2026-05-28 00:12:13 +0200

    feat: draw shimmering track behind navigator cubes

--- projects/blockexplorer/src/components/block-navigator/BlockNavigator.js
+++ projects/blockexplorer/src/components/block-navigator/BlockNavigator.js
@@ -15,6 +15,20 @@ export function BlockNavigator({ number, latest, onPrev, onNext }) {
   return (
     <nav className="block-navigator" aria-label="Block navigation">
       <div className="bn-stage">
+        <svg className="bn-track" viewBox="0 0 320 140" preserveAspectRatio="none" aria-hidden="true">
+          <defs>
+            <linearGradient id="bn-track-left" x1="50" y1="0" x2="160" y2="0" gradientUnits="userSpaceOnUse">
+              <stop offset="0%"   stopColor="#7898e6" stopOpacity="0" />
+              <stop offset="100%" stopColor="#7898e6" stopOpacity="0.45" />
+            </linearGradient>
+            <linearGradient id="bn-track-right" x1="160" y1="0" x2="270" y2="0" gradientUnits="userSpaceOnUse">
+              <stop offset="0%"   stopColor="#7898e6" stopOpacity="0.45" />
+              <stop offset="100%" stopColor="#7898e6" stopOpacity="0" />
+            </linearGradient>
+          </defs>
+          {canPrev && <path className="bn-track-path" d="M 50 62 Q 105 68 160 70" stroke="url(#bn-track-left)" />}
+          {canNext && <path className="bn-track-path" d="M 160 70 Q 215 68 270 62" stroke="url(#bn-track-right)" />}
+        </svg>
         {slots.map(({ id, slot, onClick }) => {
           const label =
             slot === 'left'  ? `Go to previous block ${id}` :

f43ca2e17e0c562b36a371e5bd9e14248b690900
Jorge Tome Blanco <jorge.tome@codere.com>
2026-07-19 03:00:10 +0200

    chore: remove erc20-indexer AU clone in favor of from-scratch build

--- projects/erc20-indexer/src/providers/browser.js
+++
@@ -1,7 +0,0 @@
-import { ethers } from 'ethers';
-
-// Wallet/signer. Only when MetaMask is injected.
-export function getBrowserProvider() {
-    if (!window.ethereum) throw new Error('MetaMask not detected');
-    return new ethers.BrowserProvider(window.ethereum);
-}
